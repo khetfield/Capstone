024ad463af99916bfff3645303ae9d9d1e5433e2
khetfield <163695693+khetfield@users.noreply.github.com>
2024-07-25 11:19:06 -0400

    Update Homepage.jsx

--- FinalProject/src/pages/Homepage.jsx
+++ FinalProject/src/pages/Homepage.jsx
@@ -9,28 +9,28 @@ const HomePage = () => {
           <h2 className="currentPromoHeader">Promotions and Deals!</h2>
           <div className="promotionlist">
             <div className="promoitem">
-              <h3 className="promoItemHeader">Promo 1</h3>
-              <p>Promo Details</p>
+              <h3 className="promoItemHeader">Milk</h3>
+              <p>25% off (Gallons only, excludes all other sizes)</p>
             </div>
             <div className="promoitem">
-              <h3 className="promoItemHeader">Promo 2</h3>
-              <p>Promo Details</p>
+              <h3 className="promoItemHeader">Asparagus</h3>
+              <p>Only $2.99/lb when you buy 2 or more</p>
             </div>
             <div className="promoitem">
-              <h3 className="promoItemHeader">Promo 3</h3>
-              <p>Promo Details</p>
+              <h3 className="promoItemHeader">Strawberries</h3>
+              <p>$2.99 per pound</p>
             </div>
             <div className="promoitem">
-              <h3 className="promoItemHeader">Promo 4</h3>
-              <p>Promo Details</p>
+              <h3 className="promoItemHeader">Dinner Rolls</h3>
+              <p>Buy 2 get 1 free</p>
             </div>
             <div className="promoitem">
-              <h3 className="promoItemHeader">Promo 5</h3>
-              <p>Promo Details</p>
+              <h3 className="promoItemHeader">Whipped Cream</h3>
+              <p>Save $.50 (Limit 4)</p>
             </div>
             <div className="promoitem">
-              <h3 className="promoItemHeader">Promo 6</h3>
-              <p>Promo Details</p>
+              <h3 className="promoItemHeader">Oranges</h3>
+              <p>Up to $3 off select varieties</p>
             </div>
           </div>
         </section>
